af32b099f7ba85c6864fc693b1e8b708924ed206
Phish Bug <kunalthool9@gmail.cm>
2024-10-21 20:25:26 +0530

    Deploying changes for Go On  2024-10-21 20:25:26

--- complete_deploy_to_dev.sh
+++ complete_deploy_to_dev.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo "Building Go Build"
+# Define colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+echo -e "$GREEN Building Go Build"
 
 go build
 
