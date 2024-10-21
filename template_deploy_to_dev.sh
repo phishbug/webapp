03de58f752df9644457a3bce6cd1fca2ede72f23
Phish Bug <kunalthool9@gmail.cm>
2024-10-21 20:36:10 +0530

    Deploying changes for pages Go Templates On

--- template_deploy_to_dev.sh
+++ template_deploy_to_dev.sh
@@ -9,10 +9,29 @@ if [ $# -lt 1 ]; then
     echo "No Parms provided if want to run from params eg: ./template_deploy_to_dev.sh --file=view"
     echo "Prociding with all template, pages, static files"
 
+    echo "Adding Git Chages"
+    git add .
+
+    echo "Git Commit"
+    git commit -m "Deploying changes for Go Templates On  $CURRENT_DATETIME"
+
+    echo "Git Push"
+    git push origin  main
+
+
     scp -r  -i "/home/kunal/pems/phisbug.pem" view  ec2-user@ec2-52-53-78-232.us-west-1.compute.amazonaws.com:~/
     scp -r  -i "/home/kunal/pems/phisbug.pem" pages  ec2-user@ec2-52-53-78-232.us-west-1.compute.amazonaws.com:~/
     scp -r  -i "/home/kunal/pems/phisbug.pem" static  ec2-user@ec2-52-53-78-232.us-west-1.compute.amazonaws.com:~/
 elif [ "$PARAM" == "view" ] || [ "$PARAM" == "pages" ] || [ "$PARAM" == "static" ]; then
+
+    echo "Adding Git Chages"
+    git add .
+
+    echo "Git Commit"
+    git commit -m "Deploying changes for $PARAM Go Templates On  $CURRENT_DATETIME"
+
+    echo "Git Push"
+    git push origin  main
 
     echo "Param Found: $PARAM"
 
